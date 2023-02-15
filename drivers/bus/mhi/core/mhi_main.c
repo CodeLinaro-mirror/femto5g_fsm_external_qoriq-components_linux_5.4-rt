f6cf28352f706e40f225b655b46fa2f1b9328ee5
Che-Min Hsieh <quic_cheminh@quicinc.com>
2023-02-15 08:29:14 -0500

    mhi: Address Static Analysis(SA) issue
    Address Static Analysis(SA) issues.
    
    Change-Id: Ife8699d3098929a43b025fb02ce0ae4bc0d5a993
    CRs-Fixed: 3406297
    Signed-off-by: <quic_cheminh@quicinc.com>

--- drivers/bus/mhi/core/mhi_main.c
+++ drivers/bus/mhi/core/mhi_main.c
@@ -1,5 +1,5 @@
 /* Copyright (c) 2018-2019, 2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -485,6 +485,7 @@ int mhi_queue_skb(struct mhi_device *mhi_dev,
 	bool frag_array;
 	int frag_index;
 
+	MHI_ASSERT(skb == NULL, "mhi_queue_skb: skb NULL");
 	fskb = skb_shinfo(skb)->frag_list;
 	while (fskb != NULL) {
 		nseg += 1;
